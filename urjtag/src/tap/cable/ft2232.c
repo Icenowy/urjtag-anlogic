43bbb8eefb2a756a38ce0a572c70995b6fd6b369
Steve Tell <tell@telltronics.org>
2017-06-30 18:12:44 -0400

    changing TCK frequency of ARM-USB-OCD-H
    point to ft2232h_set_frequency() variant to avoid TCK being 5x faster
    than the user's desired new frequency

--- urjtag/src/tap/cable/ft2232.c
+++ urjtag/src/tap/cable/ft2232.c
@@ -2349,7 +2349,7 @@ const urj_cable_driver_t urj_tap_cable_ft2232_armusbtiny_h_driver = {
     ft2232_cable_free,
     ft2232_armusbtiny_h_init,
     ft2232_armusbocd_done,
-    ft2232_set_frequency,
+    ft2232h_set_frequency,
     ft2232_clock,
     ft2232_get_tdo,
     ft2232_transfer,
